d25d1f2f2594a74bd86844847751c190345afebe
Ivan Olmo <ivan.olmo@gmail.com>
2021-01-26 17:06:50 -0600

    Changed the provided function to a pure function

--- intro_to_fp/foundation_concepts.js
+++ intro_to_fp/foundation_concepts.js
@@ -1,9 +1,13 @@
 // Directions: Translate from normal function to pure function
 let greeting = 'Hello, '
 
+// const greet = (name) => {
+//     return greeting += name
+// }
+
 const greet = (name) => {
-    return greeting += name
-}
+    return greeting + `${name}`
+};
 
 // This code looks fine at first, but what if we needed to use this greet function again? 
 // The greet function has unexpected results if used more than once because the greeting 
