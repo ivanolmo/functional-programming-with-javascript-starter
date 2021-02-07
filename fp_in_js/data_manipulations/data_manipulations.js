86b93ef14c91ecdfeaeb2487794856fef2cf348a
Ivan Olmo <ivan.olmo@gmail.com>
2021-02-07 17:19:21 -0600

    Add helper function that takes the provided json data and returns an array of objects

--- fp_in_js/data_manipulations/data_manipulations.js
+++ fp_in_js/data_manipulations/data_manipulations.js
@@ -1,5 +1,16 @@
 var nearEarthObjects = require('./nasa_near_earth_object_API.json');
 
+// helper function to pull all objects from json and push them into an array
+const jsonToArray = (data) => {
+    let allObj = [];
+    for (subList in data.near_earth_objects) {
+        for (item of data.near_earth_objects[subList]) {
+            allObj.push(item);
+        }
+    }
+    return allObj;
+};
+
 // The object in the nasa_near_earth_object_API.json is a copy of real API response from the NASA Near-Earth Object API. 
 // Find the following from the API:
 
@@ -7,6 +18,7 @@ var nearEarthObjects = require('./nasa_near_earth_object_API.json');
 // 1. How many near-earth objects did NASA register for the date of the search? Return the asteroid count.
 const totalNEO = nearEarthObjects.element_count;
 // console.log(`The total number of near earth objects in this dataset is ${totalNEO}.`)
+
 
 // Averages ------------------------------------------------
 // 2. What was the average absolute magnitude of all the near earth objects in this data set? Return the average absolute_magnitude_h.
@@ -17,11 +29,10 @@ const averageAbsMag = (data) => {
         for (item of data.near_earth_objects[subList]) {
             totalMagnitude += item.absolute_magnitude_h;
             i += 1;
-        } 
+        };
     };
-
     return totalMagnitude / i;
-}
+};
 
 const totalAverageMagnitude = averageAbsMag(nearEarthObjects);
 console.log(`The total average magnitude of all near earth objects in the dataset is ${totalAverageMagnitude}`);
@@ -35,8 +46,8 @@ const findHazardousObjects = (data) => {
         for (item of data.near_earth_objects[subList]) {
             potentialNEO.push(item);
         }
-    }
-    let hazObj = potentialNEO.filter(item => item.is_potentially_hazardous_asteroid === true)
+    };
+    let hazObj = potentialNEO.filter(item => item.is_potentially_hazardous_asteroid === true);
     let finalResult = hazObj.map(item => (
         {
             id: item.id,
@@ -44,12 +55,13 @@ const findHazardousObjects = (data) => {
             maxSize: item.estimated_diameter.miles.estimated_diameter_max,
             closestApproach: item.close_approach_data[0].miss_distance.miles
         }
-    ))
+    ));
     return finalResult
 };
 
 const allDangerousObjects = findHazardousObjects(nearEarthObjects);
 console.log(allDangerousObjects);
+
 
 // Too Close for Comfort -----------------------------------
 // 4. A list of all objects (their id, name, max size in miles, and closest approach in miles) that have a miss_distance of less than 900,000 miles
@@ -57,4 +69,3 @@ console.log(allDangerousObjects);
 
 // Alert ---------------------------------------------------
 // 5. Of all the near-earth objects for this date, find the time that the asteroid with the nearest miss will be closest to earth. 
-
