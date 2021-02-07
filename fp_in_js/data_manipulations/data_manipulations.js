43870de1b2f47ef9b86bb0af93c44f45c9ef1d5d
Ivan Olmo <ivan.olmo@gmail.com>
2021-02-07 15:30:55 -0600

    Add function to iterate through provided dataset, find items flagged as potentially dangerous, and return those items

--- fp_in_js/data_manipulations/data_manipulations.js
+++ fp_in_js/data_manipulations/data_manipulations.js
@@ -6,7 +6,7 @@ var nearEarthObjects = require('./nasa_near_earth_object_API.json');
 // Total Count ---------------------------------------------
 // 1. How many near-earth objects did NASA register for the date of the search? Return the asteroid count.
 const totalNEO = nearEarthObjects.element_count;
-console.log(`The total number of near earth objects in this dataset is ${totalNEO}.`)
+// console.log(`The total number of near earth objects in this dataset is ${totalNEO}.`)
 
 // Averages ------------------------------------------------
 // 2. What was the average absolute magnitude of all the near earth objects in this data set? Return the average absolute_magnitude_h.
@@ -27,14 +27,29 @@ const totalAverageMagnitude = averageAbsMag(nearEarthObjects);
 console.log(`The total average magnitude of all near earth objects in the dataset is ${totalAverageMagnitude}`);
 
 
-
-// Hint - you can achieve this multiple ways, but the reduce method can be a little-known but cool way to find averages. To do it though, you'll need to use the initial_value argument
-// For some extra challenge try using reduce with the initial setting argument. To learn more about it, take a look at this page: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-
-
 // Hazardous -----------------------------------------------
 // 3. A list of all objects (their id, name, max size in miles, and closest approach in miles) that are labeled potentially hazardous
+const findHazardousObjects = (data) => {
+    let potentialNEO = [];
+    for (subList in data.near_earth_objects) {
+        for (item of data.near_earth_objects[subList]) {
+            potentialNEO.push(item);
+        }
+    }
+    let hazObj = potentialNEO.filter(item => item.is_potentially_hazardous_asteroid === true)
+    let finalResult = hazObj.map(item => (
+        {
+            id: item.id,
+            name: item.name,
+            maxSize: item.estimated_diameter.miles.estimated_diameter_max,
+            closestApproach: item.close_approach_data[0].miss_distance.miles
+        }
+    ))
+    return finalResult
+};
 
+const allDangerousObjects = findHazardousObjects(nearEarthObjects);
+console.log(allDangerousObjects);
 
 // Too Close for Comfort -----------------------------------
 // 4. A list of all objects (their id, name, max size in miles, and closest approach in miles) that have a miss_distance of less than 900,000 miles
